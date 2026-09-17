86c5887c16d057d5ef0bd8953b6c315e333ee2e2
hanenhikal-rgb <hanenhikal@gmail.com>
2026-09-17 17:22:20 +0300

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,54 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+if face_cascade.empty():
+    print("الملف مش موجود أو المسار غلط!")
+else:
+    print("تمام، الموديل اتحمل بنجاح!")
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Could not access the camera.")
+        break
+
+    frame = cv2.flip(frame, 1)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5
+    )
+
+    for x, y, w, h in faces:
+        cv2.rectangle(
+            frame,
+            (x, y),
+            (x + w, y + h),
+            (0, 255, 0),
+            2
+        )
+
+    cv2.putText(
+        frame,
+        f"Faces: {len(faces)}",
+        (20, 40),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2
+    )
+
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
